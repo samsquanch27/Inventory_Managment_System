94275c3d5bd66b89ffeb14379d9a53a177777b1c
ash2042987 <32085133+ash2042987@users.noreply.github.com>
2019-02-23 20:11:57 -0800

    Add files via upload
    Java Files for Group Project Practice

---
+++ PurchasesDao.java
@@ -0,0 +1,38 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class PurchasesDao {
+
+		private Connection connection;
+		
+		private final String CREATE_NEW_PURCHASE_QUERY = "insert into purchases(name) value(?)";
+		private final String DELETE_PURCHASE_QUERY ="DELETE from IncomingPurchases WHERE id = ?";
+		
+		public PurchasesDao() {
+			connection = DBConnection.getConnection();
+		}
+		public void createNewPurchase (String purchaseName) throws SQLException {
+			PreparedStatement ps = connection.prepareStatement(CREATE_NEW_PURCHASE_QUERY);
+			ps.setString(1, purchaseName);
+			ps.executeUpdate();
+		}
+		public static Object getOrdersId(int id) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+		public void displayInventory(int id) throws SQLException {
+			PreparedStatement  ps = connection.prepareStatement(DISPLAY_CURRENT_INVENTORY_QUERY);
+			ps.setInt(1, id);
+			ps.executeUpdate();
+			}
+		
+		public void deletePurchaseByPurchaseId(int PurchaseId) throws SQLException {
+			PreparedStatement ps = connection.prepareStatement(DELETE_PURCHASE_QUERY);
+			ps.setInt(2, PurchaseId);
+			ps.executeUpdate();
+		}
+}
